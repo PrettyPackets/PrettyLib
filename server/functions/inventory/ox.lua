911dd83832a3bffcc1bd3c9d917b64b141054c48
Haptic Sys Dev <209319117+PrettyPackets@users.noreply.github.com>
2025-05-09 12:42:57 +0100

    More ox Functions

--- server/functions/inventory/ox.lua
+++ server/functions/inventory/ox.lua
@@ -1,3 +1,5 @@
+if Config.Inventory ~= 'ox' then return end
+
 function functions.HasItem(src, item, amount)
     local item_count = exports.ox_inventory:GetItemCount(src, item)
     if Config.Debug then
@@ -10,6 +12,11 @@ function functions.HasItem(src, item, amount)
     end
 end exports('HasItem', HasItem)
 
+function functions.ItemSlotSearch(src, item)
+    local item_slots = exports.ox_inventory:Search(src, 'slots', item)
+    return item_slots
+end exports('ItemSlotSearch', ItemSlotSearch)
+
 
 
 function functions.RemoveItem(src, item, amount, reason)
@@ -20,8 +27,31 @@ function functions.RemoveItem(src, item, amount, reason)
     return remove_item
 end exports('RemoveItem', RemoveItem)
 
+function functions.RemoveItemSlot(src, item, amount, slot, reason)
+    local remove_item = exports.ox_inventory:RemoveItem(src, item, amount, nil, slot)
+    if Config.Debug then
+        print("PrettyLib - RemoveItemSlot - " .. item .. " - " .. amount .. " - Results: " .. tostring(remove_item))
+    end
+    return remove_item
+end exports('RemoveItemSlot', RemoveItemSlot)
 
-if Config.Inventory ~= 'ox' then return end
+function functions.AddItem(src, item, amount, reason)
+    local add_item = exports.ox_inventory:AddItem(src, item, amount)
+    if Config.Debug then
+        print("PrettyLib - AddItem - " .. item .. " - " .. amount .. " - Results: " .. tostring(remove_item))
+    end
+    return add_item
+end exports('AddItem', AddItem)
+
+function functions.AddItemMeta(src, item, amount, meta, reason)
+    local add_item = exports.ox_inventory:AddItem(src, item, amount, meta)
+    if Config.Debug then
+        print("PrettyLib - AddItem - " .. item .. " - " .. amount .. " - Results: " .. tostring(remove_item))
+    end
+    return add_item
+end exports('AddItemMeta', AddItemMeta)
+
+
 
 function functions.DynamicStash(src, id, slots, weight, label)
     if Config.Debug then
